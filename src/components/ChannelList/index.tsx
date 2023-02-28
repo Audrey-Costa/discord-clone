19ebf5a3d368cbd1c236fe6b97cb667a13b7d27c
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-02-28 03:10:32 -0300

    Fix multiple render

--- src/components/ChannelList/index.tsx
+++ src/components/ChannelList/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ChannelButton from "../ChannelButton/index";
 import { Container, Category, AddCategoryIcon } from "../ChannelList/styles";
+import { ChannelContext } from "../../context/ChannelContext";
 
 const ChannelList: React.FC = () => {
+  const { channel } = React.useContext(ChannelContext);
   return (
     <Container>
       <Category>
@@ -10,11 +12,11 @@ const ChannelList: React.FC = () => {
         <AddCategoryIcon />
       </Category>      
 
-      <ChannelButton channelName="chat-livre" channelDescription="Canal aberto para conversas" />
-      <ChannelButton channelName="Trabalho" channelDescription="" />
-      <ChannelButton channelName="Lolzinho" channelDescription="Só pode falar de LOL!" />
-      <ChannelButton channelName="Animes" channelDescription="Sem Spoilers!" />
-      <ChannelButton channelName="Jogos" channelDescription="Vamos jogar o que hoje?" />
+      <ChannelButton channelName="chat-livre" channelDescription="Canal aberto para conversas" selected={channel.name === "chat-livre" }/>
+      <ChannelButton channelName="Trabalho" channelDescription="" selected={channel.name === "Trabalho" }/>
+      <ChannelButton channelName="Lolzinho" channelDescription="Só pode falar de LOL!" selected={channel.name === "Lolzinho" }/>
+      <ChannelButton channelName="Animes" channelDescription="Sem Spoilers!" selected={channel.name === "Animes" }/>
+      <ChannelButton channelName="Jogos" channelDescription="Vamos jogar o que hoje?" selected={channel.name === "Jogos" }/>
     </Container>
   );
 }
